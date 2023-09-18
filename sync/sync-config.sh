3ffccdf980b3fbb28bd74b6dd5e6810a02205f4c
Ashton Lance <ashton@simplefocus.com>
2023-09-18 11:23:32 -0400

    Update sync config and add missing fieldset

--- sync/sync-config.sh
+++ sync/sync-config.sh
@@ -1,4 +1,4 @@
 prodenv="sfexample"                     # WP Engine production env name
 stagingenv="ncccsstg"               # WP Engine staging env name
 localenv="ncccs-api"                      # Local environment name
-localurl="http://ncccs-api.ddev.site"     # DDEV local URL
+localurl="https://ncccs-api.ddev.site"     # DDEV local URL
