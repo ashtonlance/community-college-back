a0485c01cbb45618f99e2089249967617b3d20af
Ashton Lance <ashton@simplefocus.com>
2023-09-22 10:00:56 -0400

    Hide default "Post" type from admin

--- wp-content/themes/ncccs/custom-post-types.php
+++ wp-content/themes/ncccs/custom-post-types.php
@@ -75,4 +75,23 @@ add_action( 'init', function() {
     register_custom_post_type('apprenticeship-opp', 'Apprenticeship Opportunity', 'Apprenticeship Opportunities', '/students/what-we-offer/apprenticeships/apprenticeship-opportunities', 'dashicons-hammer', 'app-opp-program-areas', 'Apprenticeship Opportunity Program Area', 'Apprenticeship Opportunities Program Areas');
     // Add more post types here
 });
+
+// Remove default post type
+function remove_default_post_type($args, $postType) {
+    if ($postType === 'post') {
+        $args['public']                = false;
+        $args['show_ui']               = false;
+        $args['show_in_menu']          = false;
+        $args['show_in_admin_bar']     = false;
+        $args['show_in_nav_menus']     = false;
+        $args['can_export']            = false;
+        $args['has_archive']           = false;
+        $args['exclude_from_search']   = true;
+        $args['publicly_queryable']    = false;
+        $args['show_in_rest']          = false;
+    }
+
+    return $args;
+}
+add_filter('register_post_type_args', 'remove_default_post_type', 0, 2);
 ?>
