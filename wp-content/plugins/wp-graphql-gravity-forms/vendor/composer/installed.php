4c2d3519e8ea9d6989cdc5d278e9e86a7703ee03
Ashton Lance <ashton@simplefocus.com>
2023-10-02 19:54:37 -0400

    Chore: Updae WPQL GF plugin

--- wp-content/plugins/wp-graphql-gravity-forms/vendor/composer/installed.php
+++ wp-content/plugins/wp-graphql-gravity-forms/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'harness-software/wp-graphql-gravity-forms',
-        'pretty_version' => 'v0.12.3',
-        'version' => '0.12.3.0',
-        'reference' => '07d6471beff1304aa7e8db183ec63df20ab89800',
+        'pretty_version' => 'v0.12.4',
+        'version' => '0.12.4.0',
+        'reference' => '74569d87e8972289c9e5b8cc87ebb06f1bb6e134',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,18 +11,18 @@
     ),
     'versions' => array(
         'harness-software/wp-graphql-gravity-forms' => array(
-            'pretty_version' => 'v0.12.3',
-            'version' => '0.12.3.0',
-            'reference' => '07d6471beff1304aa7e8db183ec63df20ab89800',
+            'pretty_version' => 'v0.12.4',
+            'version' => '0.12.4.0',
+            'reference' => '74569d87e8972289c9e5b8cc87ebb06f1bb6e134',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
             'dev_requirement' => false,
         ),
         'yahnis-elsts/plugin-update-checker' => array(
-            'pretty_version' => 'v4.13',
-            'version' => '4.13.0.0',
-            'reference' => '6eb27a6911e0e0880d09e5b11f577d3f688f7da7',
+            'pretty_version' => 'v5.2',
+            'version' => '5.2.0.0',
+            'reference' => '5a270988c5f76bfdfbbb42cccc7c9627f7dd64d0',
             'type' => 'library',
             'install_path' => __DIR__ . '/../yahnis-elsts/plugin-update-checker',
             'aliases' => array(),
