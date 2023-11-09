1213c66a347ec661b2707eb72eb869ca0a1bb804
Ashton Lance <ashton@simplefocus.com>
2023-11-08 19:52:30 -0500

    fix: Update numbered memos slug

--- wp-content/themes/ncccs/custom-post-types.php
+++ wp-content/themes/ncccs/custom-post-types.php
@@ -98,7 +98,7 @@ add_action( 'init', function() {
     register_custom_post_type('program-areas', 'Program Area', 'Program Areas', '/students/what-we-offer/program-areas', 'dashicons-clipboard');
     register_custom_post_type('programs', 'Program', 'Programs', '/students/what-we-offer/programs', 'dashicons-book', 'program-areas', 'Tagged Program Area', 'Tagged Program Areas', 'colleges', 'College', 'Colleges');
     register_custom_post_type('annual-reports', 'Annual Reporting Plan', 'Annual Reporting Plans', '/college-faculty-staff/policy-legal-support/annual-reporting-plans', 'dashicons-chart-bar');
-    register_custom_post_type('numbered-memos', 'Numbered Memo', 'Numbered Memos', '/about-us/system-office/numbered-memos', 'dashicons-portfolio', 'numbered-memo-categories', 'Numbered Memo Category', 'Numbered Memo Categories');
+    register_custom_post_type('numbered-memos', 'Numbered Memo', 'Numbered Memos', '/about-us/numbered-memos', 'dashicons-portfolio', 'numbered-memo-categories', 'Numbered Memo Category', 'Numbered Memo Categories');
     register_custom_post_type('staff', 'Staff', 'Staff', '/about-us/system-office/staff', 'dashicons-groups', 'organization', 'Organization', 'Organizations');
     register_custom_post_type('board-members', 'Board Member', 'Board Members', '/about-us/board-members', 'dashicons-groups', 'board-members-categories', 'Board Members Category', 'Board Members Categories');
     register_custom_post_type('events', 'Event', 'Events', '/events', 'dashicons-calendar-alt', 'events-categories', 'Event Category', 'Events Categories', 'events-tags', 'Event Tag', 'Events Tags');
